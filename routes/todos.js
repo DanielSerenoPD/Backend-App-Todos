8315487dce9cfbcdd94154b534e10ff2764098d0
YoungFanPD <danielsereno432s@gmail.com>
2021-10-11 23:44:58 -0500

    feature: creacion de la ruta todos
    *permite realizar una peticion  del tipo post en la ruta /todos

--- routes/todos.js
+++ routes/todos.js
@@ -2,11 +2,18 @@ const todos = [{id: 1, title:'go to the gym'},{id: 2, title:'go to the cinema'},
 const express = require('express');
 const router = express.Router();
 
+router.use(express.json());
 router.get('/',(req, res)=>{
     res.json(todos);
 });
 router.get('/:id',(req,res)=>{
-    res.json(todos.find(todo=>todo.id === parseInt(req.params.id)));
+    const todo = todos.find(todo=>todo.id === parseInt(req.params.id));
+    todo&&res.json(todo);
+    res.status(404).json({message: "Todo not found."});
+});
+
+router.post('/',(req, res)=>{
+ res.status(200).json(req.body);
 });
 
 
